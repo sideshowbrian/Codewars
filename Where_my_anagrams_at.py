6ddb98a8405ac6cb0b6c8fdebc825936f32bfc99
Brian <32773834+sideshowbrian@users.noreply.github.com>
2021-03-01 09:57:19 -0700

    Create Where_my_anagrams_at.py

---
+++ Where_my_anagrams_at.py
@@ -0,0 +1,38 @@
+"""
+What is an anagram? Well, two words are anagrams of each other if they 
+both contain the same letters. For example:
+
+'abba' & 'baab' == true
+'abba' & 'bbaa' == true
+'abba' & 'abbba' == false
+'abba' & 'abca' == false
+
+Write a function that will find all the anagrams of a word from a list. 
+You will be given two inputs a word and an array with words. You should 
+return an array of all the anagrams or an empty array if there are none. 
+For example:
+
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+
+"""
+
+def anagrams(word, words):
+    OG_letters = {}
+    real_anagrams = []
+    ""
+    for keys in word:
+        OG_letters[keys] = OG_letters.get(keys,0) + 1
+    for i in words:
+        letters = {}
+        for keys in i:
+            letters[keys] = letters.get(keys,0) + 1
+        if OG_letters == letters:
+            real_anagrams.append(i)
+    print (real_anagrams)
+    return real_anagrams
+
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada'])
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])
+anagrams('laser', ['lazing', 'lazy',  'lacer'])
